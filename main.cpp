e1a313faf5451c20eec890f1e3727c3023e9d048
sfidan42 <sadettinfidan00@gmail.com>
2025-05-16 14:09:30 +0300

    insert one tiled monster

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include <raylib.h>
 #include "GameInstance.hpp"
 
-using json = nlohmann::json;
-
 int	main(void)
 {
 	GameInstance	*game = GameInstance::getInstance();
@@ -10,20 +8,27 @@ int	main(void)
 	game->setGame("configs/GameConfig.json");
 	game->setLevel("configs/Level1.json");
 
-	const int	windowW = game->get(ConfigType::GAME, "window.width").template get<const int>();
-	const int	windowH = game->get(ConfigType::GAME, "window.height").template get<const int>();
-	InitWindow(windowW, windowH, "Space Invaders Clone");
+	const int	screenW = game->get(ConfigType::GAME, "window.width").template get<const int>();
+	const int	screenH = game->get(ConfigType::GAME, "window.height").template get<const int>();
+
+	InitWindow(screenW, screenH, "Space Invaders Clone");
+
+	const float	marginV = game->get(ConfigType::GAME, "margin.vertical").template get<const float>();
+	const float	marginH = game->get(ConfigType::GAME, "margin.horizontal").template get<const float>();
+	const int	gapV = screenW * marginV;
+	const int	gapH = screenH * marginH;
+	const int	fontSize = game->get(ConfigType::GAME, "fontsize").template get<const int>();
+
+	std::string	texturePath = game->get(ConfigType::GAME, "textures.SpaceInvaders").template get<std::string>();
+	Texture2D	texture = LoadTexture(texturePath.c_str());
 
 	const int fps = game->get(ConfigType::GAME, "fps").template get<const int>();
 	SetTargetFPS(fps);
 
-	const float	marginH = game->get(ConfigType::GAME, "margin.horizontal").template get<const float>();
-	const float	marginV = game->get(ConfigType::GAME, "margin.vertical").template get<const float>();
-
-	const int	gapH = windowH * marginH;
-	const int	gapV = windowW * marginV;
-
-	const int	fontSize = game->get(ConfigType::GAME, "fontsize").template get<const int>();
+	Rectangle	monsterSource = { 100, 3200, 400, 300 };
+	Rectangle	monsterDestination = { 100, 100, 80, 60 };
+	Vector2		monsterOrigin = { 0, 0 };
+	Color		monsterTint = { 0xFF, 0xFF, 0xFF, 0xFF };
 
 	while (!WindowShouldClose())
 	{
@@ -31,12 +36,14 @@ int	main(void)
 
 		ClearBackground(RAYWHITE);
 
-		DrawText(game->get(ConfigType::GAME).dump().c_str(), gapV, gapH, fontSize, LIGHTGRAY);
-		DrawText(game->get(ConfigType::LEVEL).dump().c_str(), gapV, windowH / 2, fontSize, LIGHTGRAY);
-
+		DrawText("Let's play the game!", gapV, gapH, fontSize, LIGHTGRAY);
+		//DrawTexture(texture, screenW / 2 - texture.width / 2, screenH / 2 - texture.height / 2, WHITE);
+		DrawTexturePro(texture, monsterSource, monsterDestination, monsterOrigin, 0.0f, monsterTint);
+		//DrawTextureNPatch(texture, )
 		EndDrawing();
 	}
 
+	UnloadTexture(texture);
 	CloseWindow();
 
 	return (0);
